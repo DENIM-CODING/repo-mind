f7f78bb53011472a57016e35f5fa3d39974bcda6
DENIM <khannadenim@gmail.com>
2026-09-15 05:48:15 +0530

    database setup and providers

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "next-themes";
+import QueryProvider from "@/components/providers/query-provider";
 
 const geistMonoHeading = Geist_Mono({subsets:['latin'],variable:'--font-heading'});
 
@@ -25,8 +27,19 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
     <html
       lang="en"
       className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, geistMonoHeading.variable)}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    suppressHydrationWarning>
+      <body className="min-h-full flex flex-col">
+        <QueryProvider>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+          </QueryProvider>
+          </body>
     </html>
   );
 }
